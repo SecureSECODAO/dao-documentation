3c9907b236408ba0bb9be180ba39fe8816de1318
Hidde Heijnen <h.r.a.heijnen@students.uu.nl>
2023-05-29 11:20:44 +0200

    feat: move to npm, tailwind setup

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import Logo from "@/components/Logo";
+import LogoFull from "@/components/LogoFull";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+    <span className="flex items-center justify-center gap-x-2">
+      <Logo className="h-10 w-10 shrink-0" />
+      SecureSECO DAO Docs
+    </span>
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/shuding/nextra-docs-template",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: 'Nextra Docs Template',
+    text: <LogoFull className="w-32 h-fit shrink-0" />,
   },
-}
+};
 
-export default config
+export default config;
